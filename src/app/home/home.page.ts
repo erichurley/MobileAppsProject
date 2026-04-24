6724b1c787858c359ae1de103d8e6566aaf61a11
Eric Hurley <erichurley@gmail.com>
2026-04-24 18:08:44 +0100

    Added my-http.service.ts and implemented async get() method using CapacitorHTTP. Homepage now loads Trending Movies using ngOnInit. Homepage.html showing movie names using *ngFor on a basic ion-list.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonList } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+import { MyHttpService } from '../services/my-http.service';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLink, IonItem, IonList, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+
+  movies: any;
+
+  options: HttpOptions = {
+    url: ''
+  }
+
+  constructor(private mhs:MyHttpService) {}
+
+  ngOnInit() {
+    this.movies = [];
+    this.getTrendingMovies();
+  }
+
+  async getTrendingMovies() {
+    this.options = {
+      url: 'https://api.themoviedb.org/3/trending/movie/day?api_key=87f190fd1f3c1281452f0b3458f9401c'
+    }
+    let result = await this.mhs.get(this.options);
+    this.movies = result.data.results;
+  }
 }
