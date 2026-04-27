6788c98a2eae9821e38e05f9ae32afb20475faf3
Eric Hurley <erichurley@gmail.com>
2026-04-27 18:17:26 +0100

    Added searchMovies() function which is bound to the click event on the search button. Implemented data binding using ngModel on the searchbar text. Added string interpolation to dynamically update the homepage title.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,16 +4,20 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLis
 import { RouterLink } from '@angular/router';
 import { MyHttpService } from '../services/my-http.service';
 import { HttpOptions } from '@capacitor/core';
+import { FormsModule } from '@angular/forms';
+import { getElement } from 'ionicons/dist/types/stencil-public-runtime';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLink, IonItem, IonList, CommonModule, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonInput, IonSearchbar],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLink, IonItem, IonList, CommonModule, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonInput, IonSearchbar, FormsModule],
 })
 export class HomePage {
 
   movies: any;
+  searchString: string = '';
+  homePageTitle: string = "Today's Trending Movies:";
 
   options: HttpOptions = {
     url: ''
@@ -32,5 +36,22 @@ export class HomePage {
     }
     let result = await this.mhs.get(this.options);
     this.movies = result.data.results;
+  }
+
+  async searchMovies() {
+    if (this.searchString === '') {
+      this.homePageTitle = "Today's Trending Movies:";
+      this.getTrendingMovies();
+      return;
+    }
+
+    this.options = {
+      url: 'https://api.themoviedb.org/3/search/movie?api_key=87f190fd1f3c1281452f0b3458f9401c&query=' + this.searchString
+    }
+
+    this.homePageTitle = "Matching Search Results:";
+    let result = await this.mhs.get(this.options);
+    this.movies = result.data.results;
+
   }
 }
