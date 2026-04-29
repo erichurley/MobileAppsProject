c830a862f603b77baa6a555a8e36736694c92d58
Eric Hurley <erichurley@gmail.com>
2026-04-29 12:47:45 +0100

    Added movie-details page logic. Retrieves selected movie from shared data service using ngOnInit().

--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+import { Data } from '../services/data';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, RouterLink]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, RouterLink, IonCard, IonCardHeader, IonCardTitle, IonCardContent]
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
+  movie: any;
 
+  //Creates a private instance of data service.
+  constructor(private data:Data) { }
+
+  //Retrieves selected movie from shared data service
   ngOnInit() {
+    this.movie = this.data.clickedMovie;
   }
 
 }
