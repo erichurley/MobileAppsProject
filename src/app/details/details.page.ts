eba971a175308c70c3ca994454b06b7e7415713a
Eric Hurley <erichurley@gmail.com>
2026-04-30 16:31:44 +0100

    Added list header and deathday information. Still need to implement the other movies section.

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -12,7 +12,7 @@ import { MyHttpService } from '../services/my-http.service';
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, RouterLink, IonCard, IonList, IonItem]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, RouterLink, IonCard, IonList, IonItem, IonListHeader]
 })
 export class DetailsPage implements OnInit {
 
