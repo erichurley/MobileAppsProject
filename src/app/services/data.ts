778b9a58bccc99466daceb5c19daafaa2b93cb34
Eric Hurley <erichurley@gmail.com>
2026-05-01 14:46:58 +0100

    Ionic Storage imported into data service and async init(), set() & get() functions added.

--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +8,22 @@ export class Data {
 
   clickedMovie: any;
   clickedPerson: any;
+  favourites: any[] = [];
+
+  constructor(private storage:Storage) {
+    this.init();
+  }
+
+  async init() {
+    await this.storage.create();
+  }
+
+  async set(key: string, value: any) {
+    await this.storage.set(key, value);
+  }
+
+  async get(key: string) {
+    return await this.storage.get(key);
+  }
   
 }
