bc192d89f3beb4600a1e696adfa435b17332f3b4
Eric Hurley <erichurley@gmail.com>
2026-05-01 19:19:47 +0100

    Implemented Favourites page. Shows ion-card for all movies in favourites. Added buttons to remove all favourites, and remove single movies. Button logic not yet implemented.

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonIcon } from '@ionic/angular/standalone';
+import { Data } from '../services/data';
+import { addIcons } from 'ionicons';
+import { trashOutline } from 'ionicons/icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonIcon]
 })
 export class FavouritesPage implements OnInit {
 
-  constructor() { }
+  favourites: any[] = [];
+
+  constructor(private data: Data, private router:Router) {
+    addIcons({trashOutline});
+  }
 
   ngOnInit() {
+  }
+
+  async ionViewWillEnter() {
+  await this.data.loadFavourites();
+  this.favourites = this.data.favourites;
+}
+
+//Stores the selected movie in the data service and navigates to the movie-details page. 
+  clickMovie(movie: any) {
+    this.data.clickedMovie = movie;
+    this.router.navigate(['/movie-details']);
   }
 
 }
